01b851422d88cf64b992ab96009ad09fc45b1ad2
Davenchy <firon1222@gmail.com>
2018-07-28 15:49:11 +0200

    add socket.io and connect, disconnect events handlers

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+io.on('connection', (socket) => {
+    console.log('user was connected');
+
+    socket.on('disconnect', (socket) => {
+        console.log('user was disconnected');
+    });
+});
 
 
-app.listen(PORT, () => {
+
+
+server.listen(PORT, () => {
     console.log(`server is up on port ${PORT}`);
 });
