c3e291cb004c6b930e6297dcec1f38d73a2c2ff5
Davenchy <firon1222@gmail.com>
2018-07-29 12:50:46 +0200

    add jquery and fix some bugs

---
+++ public/js/chat.js
@@ -0,0 +1,48 @@
+var socket = io();
+var name = 'User';
+
+// log on connected with server
+socket.on('connect', function () {
+    console.log("connected to server");
+});
+
+// log on disconnected with server
+socket.on('disconnect', function () {
+    console.log("disconnected from server");
+});
+
+
+socket.on('newMessage', function (message) {
+    createMessage(message.from, message.text);
+});
+
+// on document ready
+$(document).ready(function () {
+    name = prompt("What is your name?", "User");
+
+    // on messages form submit new message
+    $('form').on('submit', function (e) {
+        console.log('hello');
+        // prevent page refresh
+        e.preventDefault();
+        // get the message
+        var msg = $('[name=message]').val();
+        // clear the text input
+        $('[name=message]').val('');
+        // emit the message to the server
+        socket.emit('createMessage', {
+            from: name,
+            text: msg
+        });
+        // print message to the screen
+        createMessage('me', msg);
+    });
+
+});
+
+
+function createMessage(from, text) {
+    var msg = $('<p></p>');
+    msg.text(`${from}: ${text}`);
+    $('#messages').append(msg);
+}
